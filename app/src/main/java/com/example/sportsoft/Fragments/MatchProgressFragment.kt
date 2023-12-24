c452b26e340e60ad426e99e0c380fb66c91fa047
Pikosha2101 <Puka4yBTanke@gmail.com>
2023-12-25 02:27:03 +0300

    .

--- app/src/main/java/com/example/sportsoft/Fragments/MatchProgressFragment.kt
+++ app/src/main/java/com/example/sportsoft/Fragments/MatchProgressFragment.kt
@@ -12,6 +12,7 @@ import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.PopupMenu
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.sportsoft.CountUpTimer
@@ -53,27 +54,54 @@ class MatchProgressFragment : Fragment(R.layout.match_progress_fragment) {
             } else if (isTimerPaused) {
                 resumeTimer()
             }
-            startCardView.strokeColor = resources.getColor(R.color.unavailableText)
-            startTimeConstraintLayout.setBackgroundColor(resources.getColor(white))
-            startTimerTextView.setTextColor(resources.getColor(R.color.unavailableText))
+
+            startCardView.strokeColor = ContextCompat.getColor(requireContext(), R.color.unavailableText)
+            startTimeConstraintLayout.setBackgroundColor(ContextCompat.getColor(requireContext(), android.R.color.white))
+            startTimerTextView.setTextColor(ContextCompat.getColor(requireContext(), R.color.unavailableText))
             startImageView.setImageResource(R.drawable.arrow_start_icon_unavailable)
+
+            pauseConstraintLayout.setBackgroundColor(ContextCompat.getColor(requireContext(), blue))
+            pauseCardView.strokeColor = ContextCompat.getColor(requireContext(), blue)
+            pauseButtonImageView.setImageResource(R.drawable.pause_availabe_icon)
+            pauseTextView.setTextColor(ContextCompat.getColor(requireContext(), android.R.color.white))
+
+            stopConstraintLayout.setBackgroundColor(ContextCompat.getColor(requireContext(), blue))
+            stopCardView.strokeColor = ContextCompat.getColor(requireContext(), blue)
+            stopTextView.setTextColor(ContextCompat.getColor(requireContext(), android.R.color.white))
+            stopButtonImageView.setImageResource(R.drawable.stop_available_icon)
         }
 
         stopCardView.setOnClickListener {
             stopTimer()
-            startCardView.strokeColor = resources.getColor(R.color.blue)
-            startTimeConstraintLayout.setBackgroundColor(resources.getColor(blue))
-            startTimerTextView.setTextColor(resources.getColor(R.color.white))
+            startCardView.strokeColor = ContextCompat.getColor(requireContext(), blue)
+            startTimeConstraintLayout.setBackgroundColor(ContextCompat.getColor(requireContext(), blue))
+            startTimerTextView.setTextColor(ContextCompat.getColor(requireContext(), android.R.color.white))
             startImageView.setImageResource(R.drawable.arrow_start_icon)
+
+            stopConstraintLayout.setBackgroundColor(ContextCompat.getColor(requireContext(), android.R.color.white))
+            stopCardView.strokeColor = ContextCompat.getColor(requireContext(), R.color.unavailableText)
+            stopTextView.setTextColor(ContextCompat.getColor(requireContext(), R.color.unavailableText))
+            stopButtonImageView.setImageResource(R.drawable.stop_unavailable_icon)
+
+            pauseConstraintLayout.setBackgroundColor(ContextCompat.getColor(requireContext(), android.R.color.white))
+            pauseCardView.strokeColor = ContextCompat.getColor(requireContext(), R.color.unavailableText)
+            pauseButtonImageView.setImageResource(R.drawable.pause_unavailabe_icon)
+            pauseTextView.setTextColor(ContextCompat.getColor(requireContext(), R.color.unavailableText))
         }
 
         pauseCardView.setOnClickListener {
             pauseTimer()
-            startCardView.strokeColor = resources.getColor(R.color.blue)
-            startTimeConstraintLayout.setBackgroundColor(resources.getColor(blue))
-            startTimerTextView.setTextColor(resources.getColor(R.color.white))
+            startCardView.strokeColor = ContextCompat.getColor(requireContext(), blue)
+            startTimeConstraintLayout.setBackgroundColor(ContextCompat.getColor(requireContext(), blue))
+            startTimerTextView.setTextColor(ContextCompat.getColor(requireContext(), android.R.color.white))
             startImageView.setImageResource(R.drawable.arrow_start_icon)
+
+            pauseConstraintLayout.setBackgroundColor(ContextCompat.getColor(requireContext(), android.R.color.white))
+            pauseCardView.strokeColor = ContextCompat.getColor(requireContext(), R.color.unavailableText)
+            pauseButtonImageView.setImageResource(R.drawable.pause_unavailabe_icon)
+            pauseTextView.setTextColor(ContextCompat.getColor(requireContext(), R.color.unavailableText))
         }
+
 
         finishCardView.setOnClickListener {
             findNavController().navigate(R.id.action_matchProgressFragment_to_matchRegisterFragment)
